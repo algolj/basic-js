9a0a4c823910889b37ca9bf90f13bedc824b2e47
Oleksandr <algolj@algolj.it>
2021-09-13 11:56:16 +0300

    add st-dns

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -22,7 +22,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+  return domains.reduce((dns, el) => {
+    el.split('.')
+      .reverse()
+      .reduce((acc, vl) => {
+        acc += '.' + vl;
+        dns?.[acc] !== undefined ? dns[acc]++ : (dns[acc] = 1);
+        return acc;
+      }, '');
+    return dns;
+  }, {});
 }
