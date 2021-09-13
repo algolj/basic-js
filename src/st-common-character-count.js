a458de5060653ec576b00ed1860b0c4ab95cb28c
Oleksandr <algolj@algolj.it>
2021-09-13 11:53:12 +0300

    add st-common-character-count

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+  return [...s2].reduce(
+    (acc, el) => {
+      if (acc[0].includes(el)) {
+        acc[0].splice(acc[0].indexOf(el), 1);
+        acc[1]++;
+      }
+      return acc;
+    },
+    [[...s1], 0]
+  )[1];
 }
