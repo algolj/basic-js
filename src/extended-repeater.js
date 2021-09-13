89717ff20e12f68c58445eb57e802967b612cd31
Oleksandr <algolj@algolj.it>
2021-09-13 11:47:59 +0300

    add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,20 +2,33 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  const supRepeator = (st = '', rep = 1, repStr) =>
+    `${String(st)}${repStr}`.repeat(rep).slice(0, -repStr.length);
+
+  return supRepeator(
+    str +
+      (options?.addition != undefined || options?.additionSeparator != undefined
+        ? supRepeator(
+            options?.addition,
+            options?.additionRepeatTimes,
+            options?.additionSeparator || '|'
+          )
+        : ''),
+    options?.repeatTimes,
+    options?.separator || '+'
+  );
 }
