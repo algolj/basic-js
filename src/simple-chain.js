f37ce9ad2e6958f8138b4519f92eabd8bf863d60
Oleksandr <algolj@algolj.it>
2021-09-13 11:51:53 +0300

    add chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,27 +2,40 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 export default {
+  chains: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chains.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+    this.chains.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      typeof position != 'number' ||
+      position - 1 >= this.chains.length ||
+      position - 1 < 0 ||
+      position % 1
+    ) {
+      this.chains = [];
+      throw new Error(`You can't remove incorrect link!`);
+    } else {
+      this.chains.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chains.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    return (
+      this.chains
+        .splice(0, this.chains.length)
+        .reduce((acc, el) => `${acc}${acc ? `~~` : ''}( ${el} )`, '') || `( )`
+    );
+  },
 };
