9656e754c6339224841b851a483bafab895df9d5
Oleksandr <algolj@algolj.it>
2021-09-13 11:46:33 +0300

    add team

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,18 +2,35 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// check each element of the members
+// that it is a string and she starts with a capital letter
+// if the element matches the condition
+// leave only the first letter of the name
+// if the element does not match the condition,
+// change it to an empty string
+// sort the array by alphavite
+// after concatenating the lines,
+// get the command name
+export default function createDreamTeam(members) {
+  return Array.isArray(members)
+    ? members
+        .map((el) =>
+          typeof el === 'string' && /[A-Za-z]/.test(el.trim()[0])
+            ? el.trim()[0].toUpperCase()
+            : ''
+        )
+        .sort()
+        .join('')
+    : false;
 }
