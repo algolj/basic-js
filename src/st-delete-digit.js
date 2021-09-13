c85c7f231c087f61ab61d08defa0b10478fd5f63
Oleksandr <algolj@algolj.it>
2021-09-13 11:54:52 +0300

    add st-delete-digit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  let nArr = String(n).split('');
+
+  if (n >= 0) {
+    if (!(n % 10)) return n / 10;
+    if (nArr.length == 2) return +nArr[nArr[0] > nArr[1] ? 0 : 1];
+
+    for (let i = 0; i < nArr.length - 1; i++) {
+      if (nArr[i] < nArr[i + 1]) {
+        nArr.splice(i, 1);
+        break;
+      }
+    }
+  } else {
+    if (!(n % 10) && nArr.length > 3) return n / 10;
+    if (nArr.length == 2) return +('-' + nArr[nArr[0] > nArr[1] ? 0 : 1]);
+
+    for (let i = 1; i < nArr.length - 1; i++) {
+      if (nArr[i] > nArr[i + 1]) {
+        nArr.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  return +nArr.join('');
 }
