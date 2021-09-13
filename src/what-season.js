0565888e05c4a47b47f7f8e2aedf77a083ae34ab
Oleksandr <algolj@algolj.it>
2021-09-13 12:09:13 +0300

    add season

--- src/what-season.js
+++ src/what-season.js
@@ -2,16 +2,34 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const getSeasonName = (seasonNumber) =>
+  seasonNumber <= 1 || seasonNumber == 11
+    ? 'winter'
+    : seasonNumber <= 4
+    ? 'spring'
+    : seasonNumber <= 7
+    ? 'summer'
+    : 'autumn';
+
+export default function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+
+  try {
+    if (new Date(JSON.parse(JSON.stringify(date))) != 'Invalid Date') {
+      return getSeasonName(date.getMonth());
+    } else {
+      throw '';
+    }
+  } catch (e) {
+    throw new Error('Invalid date!');
+  }
 }
