71d5b385da20d5eebda095f1a4c1a10524ddc45c
Oleksandr <algolj@algolj.it>
2021-09-13 12:46:55 +0300

    add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,6 +16,63 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function repeater(str, options) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  const supRepeator = (st = '', rep = 1, repStr) =>
+    `${String(st)}${repStr}`.repeat(rep).slice(0, -repStr.length);
+  let option = {};
+  if (typeof options !== 'undefined') {
+    option = {
+      addition: options.hasOwnProperty('addition') ? options.addition : '',
+      additionSeparator: options.hasOwnProperty('additionSeparator')
+        ? options.additionSeparator
+        : '|',
+      additionRepeatTimes: options.hasOwnProperty('additionRepeatTimes')
+        ? options.additionRepeatTimes
+        : 1,
+      repeatTimes: options.hasOwnProperty('repeatTimes')
+        ? options.repeatTimes
+        : 1,
+      separator: options.hasOwnProperty('separator')
+        ? (option.separator = options.separator)
+        : '+',
+    };
+  } else {
+    option = {
+      addition: '',
+      additionRepeatTimes: 1,
+      additionSeparator: '|',
+      repeatTimes: 1,
+      separator: '+',
+    };
+  }
+  return supRepeator(
+    str +
+      supRepeator(
+        option.addition,
+        option.additionRepeatTimes,
+        option.additionSeparator || '|'
+      ),
+    option.repeatTimes,
+    option.separator || '+'
+  );
 }
+
+// My first version, but the rs.school tests don't work with ?.
+/*
+export default function repeater(str, options) {
+  const supRepeator = (st = '', rep = 1, repStr) =>
+    `${String(st)}${repStr}`.repeat(rep).slice(0, -repStr.length);
+
+  return supRepeator(
+    str +
+      (options?.addition != undefined || options?.additionSeparator != undefined
+        ? supRepeator(
+            options?.addition,
+            options?.additionRepeatTimes,
+            options?.additionSeparator || '|'
+          )
+        : ''),
+    options?.repeatTimes,
+    options?.separator || '+'
+  );
+}
+*/
