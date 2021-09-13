678e008dd531137c30b3f9bd1150515b3eb09a96
Oleksandr <algolj@algolj.it>
2021-09-13 12:03:39 +0300

    add st-sort

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,25 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  // Sort arr into two arrays
+  // indexAllMinusOne includes indices of values ​​-1
+  // allOtherNumber includes all values ​​not equal to -1
+  let { indexAllMinusOne, allOtherNumber } = arr.reduce(
+    (acc, el, i) => {
+      el == -1 ? acc.indexAllMinusOne.push(i) : acc.allOtherNumber.push(el);
+      return acc;
+    },
+    { indexAllMinusOne: [], allOtherNumber: [] }
+  );
+
+  // sort all values ​​not equal to -1
+  // in ascending order
+  allOtherNumber.sort((a, b) => a - b);
+
+  // Adds to the sorted allOtherNumber array all -1
+  // by their index in the arr array
+  for (let index of indexAllMinusOne) allOtherNumber.splice(index, 0, -1);
+
+  return allOtherNumber;
 }
